d18b3069604fd91bd183617543eca7d9ae76552b
LipeAma <contato.feamaral@gmail.com>
2024-08-27 15:25:46 -0300

    lab 2 modificado

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,36 +1,44 @@
-#include <math.h>
+// MC202 - Lab02
+// Felipe Costa Amaral - Ra:249239
+// Verifica se coelhos conseguem escapar das raposas
+
 #include <stdio.h>
 
-float dist(float x1, float y1, float x2, float y2) {
-  return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
+int main(void)
+{
+  int n;                // Número de buracos
+  int escapou;          // Boleano para interrupção do loop
+  float xc, yc, xr, yr; // Coordenadas do coelho e da raposa
+  float x, y;           // Coordenadas dos buracos
+  float dc, dr;         // Quadrado da distância entre buraco e coelho/raposa
 
-int main(void) {
-  int n_buracos, escapou;
-  float x_coelho, y_coelho, x_raposa, y_raposa;
-  float x_buraco, y_buraco;
-  float d_coelho, d_raposa;
-
-  while (scanf("%d %f %f %f %f", &n_buracos, &x_coelho, &y_coelho, &x_raposa,
-               &y_raposa) == 5) {
+  while (scanf("%d %f %f %f %f", &n, &xc, &yc, &xr, &yr) == 5)
+  {
     escapou = 0;
-    for (int i = 1; i <= n_buracos; i++) {
-      scanf("%f %f", &x_buraco, &y_buraco);
+    for (int i = 1; i <= n; i++)
+    {
+      scanf("%f %f", &x, &y);
+
+      // Se o coelho já escapou, o loop só consumirá o stdin e não realizará
+      // as outras operações. A condicional abaixo garante isso.
       if (escapou)
         continue;
-      d_coelho = dist(x_coelho, y_coelho, x_buraco, y_buraco);
-      d_raposa = dist(x_raposa, y_raposa, x_buraco, y_buraco);
 
-      if (4 * d_coelho < d_raposa) {
-        printf("O coelho pode escapar pelo buraco (%.3f,%.3f)\n", x_buraco,
-               y_buraco);
+      dc = (x - xc) * (x - xc) + (y - yc) * (y - yc);
+      dr = (x - xr) * (x - xr) + (y - yr) * (y - yr);
+
+      // O coelho chega primeiro se sua distancia for metade da distancia da raposa.
+      // Isso acontece se o quadrado da sua distancia for quatro vezes menor que o
+      // quadrado da distancia da raposa. A condicional abaixo verifica isso.
+      if (4 * dc < dr)
+      {
+        printf("O coelho pode escapar pelo buraco (%.3f,%.3f).\n", x, y);
         escapou = 1;
         continue;
       }
 
-      if (i == n_buracos) {
-        printf("O coelho nao pode escapar\n");
-      }
+      if (i == n)
+        printf("O coelho nao pode escapar.\n");
     }
   }
 }
