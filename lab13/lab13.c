bbd819bf864bfda5c0626dec4d50de1218ff1bba
LipeAma <contato.feamaral@gmail.com>
2024-11-07 15:37:58 -0300

    lab13 pronto

--- lab13/lab13.c
+++ lab13/lab13.c
@@ -14,7 +14,7 @@ typedef struct {
   Person **table;
   unsigned capacity;
   unsigned size;
-} hashTable;
+} Hash;
 
 unsigned long djb2(char *str) {
   unsigned long hash = 5381;
@@ -25,114 +25,94 @@ unsigned long djb2(char *str) {
   return hash;
 }
 
-unsigned hash(char *str, unsigned n_of_collisions, unsigned table_size) {
+unsigned hashString(char *str, unsigned n_of_collisions, unsigned capacity) {
   unsigned long x = djb2(str);
-  return ((x % table_size) + n_of_collisions) % table_size;
+  return ((x % capacity) + n_of_collisions) % capacity;
 }
 
-hashTable *newHashTable(unsigned capacity) {
-  hashTable *table = calloc(1, sizeof(hashTable));
-  if (table == NULL)
+Hash *newHashTable(unsigned capacity) {
+  Hash *hash = (Hash *)calloc(1, sizeof(Hash));
+  if (hash == NULL)
     fprintf(stderr,
-            "Error while allocating memory for hashTable with size %u\n",
+            "Error while allocating memory for hashTable with capacity %u\n",
             capacity);
-  table->capacity = capacity;
-  table->size = 0;
-  table->table = calloc(capacity, sizeof(Person *));
-  return table;
+  hash->capacity = capacity;
+  hash->size = 0;
+  hash->table = (Person **)calloc(capacity, sizeof(Person *));
+  return hash;
 }
 
 Person *newPerson(char *name) {
-  Person *new_person = malloc(sizeof(Person));
+  Person *new_person = (Person *)malloc(sizeof(Person));
+
   if (new_person == NULL)
     fprintf(stderr, "Error while allocating memory for person (%s)\n", name);
+
   strcpy(new_person->name, name);
   new_person->known_by = 0;
   new_person->knows = 0;
+
   return new_person;
 }
 
-Person *increaseKnownBy(hashTable *table, char *name) {
-  // Increases the counter which tracks how many people know <name>, and then
-  // returns it.
+Person *getOrAdd(Hash *hash, char *name) {
   unsigned n_of_collisions = 0;
   unsigned key_hash;
-  Person **pperson;
+  Person **p_person;
   Person *person;
 
   while (1) {
-    key_hash = hash(name, n_of_collisions, table->capacity);
-    pperson = &table->table[key_hash];
-    person = *pperson;
+    key_hash = hashString(name, n_of_collisions, hash->capacity);
+    p_person = &hash->table[key_hash];
+    person = *p_person;
 
     if (person == NULL) {
-      *pperson = newPerson(name);
-      ++(*pperson)->known_by;
-      ++table->size;
-      return *pperson;
-    }
-
-    if (strcmp(person->name, name)) {
+      *p_person = newPerson(name);
+      ++hash->size;
+      return *p_person;
+    } else if (strcmp(person->name, name)) {
       ++n_of_collisions;
       continue;
     } else {
-      ++person->known_by;
       return person;
-    }
-  }
-}
-
-void increseKnows(hashTable *table, char *name) {
-  // Increases the counter which tracks how many people know <name>, and then
-  // returns it.
-  unsigned n_of_collisions = 0;
-  unsigned key_hash;
-  Person **pperson;
-  Person *person;
-
-  while (1) {
-    key_hash = hash(name, n_of_collisions, table->capacity);
-    pperson = &table->table[key_hash];
-    person = *pperson;
-
-    if (person == NULL) {
-      *pperson = newPerson(name);
-      ++(*pperson)->knows;
-      ++table->size;
-      return;
-    }
-
-    if (strcmp(person->name, name)) {
-      ++n_of_collisions;
-      continue;
-    } else {
-      ++person->knows;
-      return;
     }
   }
 }
 
 int main(void) {
   char str1[110];
-  Person *most_known = NULL, *person;
-  unsigned n_of_people = 0;
-  hashTable *hash_table = newHashTable(N_OF_PEOPLE * 1.5);
+  Hash *hash = newHashTable(N_OF_PEOPLE * 1.5);
+
+  // default value to prevent NULL access within loop.
+  Person *default_most_known = newPerson((char *)"default");
+  default_most_known->known_by = 0;
+  Person *most_known = default_most_known;
+
   while (scanf(" %109[^\n]", str1) != EOF) {
     char *str2 = strstr(str1, " conhece ");
     if (str2 == NULL) continue;
     str2[0] = '\0';
-    str2 += 9;
+    str2 += 9;  // size of " conhece "
 
-    increseKnows(hash_table, str1);
+    // now str1 is the first name and str2 is the second.
 
-    person = increaseKnownBy(hash_table, str2);
-    if (most_known == NULL || person->known_by > most_known->known_by) {
-      most_known = person;
+    Person *person1, *person2;
+    person1 = getOrAdd(hash, str1);
+    person2 = getOrAdd(hash, str2);
+    if (strcmp(str1, str2)) {
+      ++person1->knows;
+      ++person2->known_by;
+    }
+    if (person2->known_by >= most_known->known_by) {
+      most_known = person2;
     }
   }
-  if (most_known->knows == 0 && most_known->known_by == hash_table->size - 1) {
+
+  if (most_known->knows == 0 && most_known->known_by == hash->size - 1) {
     printf("%s e' celebridade.\n", most_known->name);
   } else {
     printf("Nao ha' celebridade.\n");
   }
+
+  // TODO: free default_most_known and free hashTable.
 }
