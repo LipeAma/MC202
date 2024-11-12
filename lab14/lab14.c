f02e56160fc678772c32edc9d366710b29201eac
LipeAma <contato.feamaral@gmail.com>
2024-11-11 21:52:25 -0300

    lab14 começado

---
+++ lab14/lab14.c
@@ -0,0 +1,84 @@
+/*
+- c n
+criar um heap vazio com n pares {chave,custo}. Se já houver um heap, deve ser
+removido e um novo heap vazio deve ser criado.
+
+- i chave custo
+Inserir o par {chave,custo} no heap indexado. Se estiver no heap, não fazer
+nada.
+
+- m
+Remover o mínimo do heap indexado e imprimir. S vazio imprimir "heap vazio". Se
+houver mais de um mínimo, o a menor chave deve ser removido.
+
+- d chave custo
+Diminuir o custo da chave.
+
+- t
+terminar, toda memória deve ser liberada.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  unsigned key;
+  int cost;
+} Pair;
+
+typedef struct {
+  Pair *pairs;
+  unsigned *index;
+} Heap;
+
+void heapFree(Heap *);
+Heap *heapNew(unsigned);
+void heapInsert(Heap *, unsigned, int);
+Pair *heapRemove(Heap *);
+void heapDecrease(Heap *, unsigned, int);
+
+int main(void) {
+  char command;
+  int cost;
+  unsigned key;
+  Heap *heap = NULL;
+
+  while (scanf(" %c", &command) != EOF) {
+    switch (command) {
+      case 'c':
+        if (heap == NULL) {
+          heapFree(heap);
+          heap = NULL;
+        }
+        scanf("%d", &cost);
+        heap = heapNew(cost);
+        break;
+
+      case 'i':
+        if (heap == NULL) continue;
+        scanf("%u %d", &key, &cost);
+        heapInsert(heap, key, cost);
+        break;
+
+      case 'm':
+        if (heap == NULL) continue;
+        Pair *pair = heapRemove(heap);
+        if (pair == NULL)
+          printf("heap vazio\n");
+        else
+          printf("minimo {%u,%d}\n", pair->key, pair->cost);
+        break;
+
+      case 'd':
+        if (heap == NULL) continue;
+        scanf("%u %d", &key, &cost);
+        heapDecrease(heap, key, cost);
+        break;
+
+      case 't':
+        if (heap == NULL) break;
+        heapFree(heap);
+        break;
+    }
+  }
+}
